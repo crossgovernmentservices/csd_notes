18d71370766fddb479b46df7081abd2726791d68
Andy Driver <andy@pagezero.net>
2016-04-05 17:58:39 +0100

    Add assets management command for precompilation

--- manage.py
+++ manage.py
@@ -8,6 +8,7 @@ from tempfile import TemporaryDirectory
 import urllib
 import zipfile
 
+from flask.ext.assets import ManageAssets
 from flask.ext.migrate import MigrateCommand
 from flask.ext.script import Manager
 import pytest
@@ -16,6 +17,7 @@ from app.factory import create_app
 
 
 manager = Manager(create_app)
+manager.add_command('assets', ManageAssets())
 manager.add_command('db', MigrateCommand)
 
 
