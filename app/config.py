16cc398a6606257e159b5ca4ff0ba27cd7b95a38
Kerin Cosford <cosford@gmail.com>
2016-05-13 13:48:05 +0100

    DB_HOST and DB_PORT were the wrong way round

--- app/config.py
+++ app/config.py
@@ -34,8 +34,8 @@ SECRET_KEY = env.get('SECRET_KEY', os.urandom(24))
 if all([env.get('DB_HOST'), env.get('DB_PORT'), env.get('DB_USERNAME'),
         env.get('DB_PASSWORD')]):
     SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://{}:{}@{}:{}/notes".format(
-        env.get('DB_USERNAME'), env.get('DB_PASSWORD'), env.get('DB_PORT'),
-        env.get('DB_HOST'))
+        env.get('DB_USERNAME'), env.get('DB_PASSWORD'), env.get('DB_HOST'),
+        env.get('DB_PORT'))
 else:
     SQLALCHEMY_DATABASE_URI = env.get('DATABASE_URL',
                                       'postgresql+psycopg2://localhost/notes')
